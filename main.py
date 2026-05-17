fd0f0d41d04d432c35368ca2ed608a294a724b4e
truly a moth <denisfedoroff@icloud.com>
2026-05-17 15:56:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from PIL import Image, ImageEnhance
+from PIL import Image, ImageEnhance, ImageOps
 
 file = None
+orig_file = None
+angle = 0
+
+def upd_effects(img):
+    new_brgt = slider.value() / 100
+    new_ctrt = slider2.value() / 100
+    new_clr = slider3.value() / 100
+
+    image_mod_b = ImageEnhance.Brightness(img)
+    mid1_image = image_mod_b.enhance(new_brgt)
+
+    image_mod_c = ImageEnhance.Contrast(mid1_image)
+    mid2_image = image_mod_c.enhance(new_ctrt)
+
+    image_mod_cl = ImageEnhance.Color(mid2_image)
+    new_image = image_mod_cl.enhance(new_clr)
+    return new_image
 
 def img_update():
     global file
     if file is None:
         return
 
-    new_brgt = slider.value() / 100
-    new_ctrt = slider2.value() / 100
-    new_clr = slider3.value() / 100
+    display_img = upd_effects(file)
+    show_img(display_img)
 
-    image_mod_b = ImageEnhance.Brightness(file)
-    mid1_image = image_mod_b.enhance(new_brgt)
-    image_mod_c = ImageEnhance.Contrast(mid1_image)
-    mid2_image = image_mod_c.enhance(new_ctrt)
-    image_mod_cl = ImageEnhance.Color(mid2_image)
-    new_image = image_mod_cl.enhance(new_clr)
-
-    rgba_img = new_image.convert("RGBA")
+def show_img(img):
+    rgba_img = img.convert("RGBA")
     data = rgba_img.tobytes("raw", "RGBA")
     q_img = QImage(data, rgba_img.size[0], rgba_img.size[1], QImage.Format_RGBA8888)
 
@@ -29,45 +39,85 @@ def img_update():
     image_label.setPixmap(pixmap)
 
 def clear_effects():
-    global file
-    if file is None:
+    global file, orig_file
+    if orig_file is None:
         return
 
-    image_mod_b = ImageEnhance.Brightness(file)
-    mid1_image = image_mod_b.enhance(1)
-    image_mod_c = ImageEnhance.Contrast(mid1_image)
-    mid2_image = image_mod_c.enhance(1)
-    image_mod_cl = ImageEnhance.Color(mid2_image)
-    new_image = image_mod_cl.enhance(1)
+    slider.blockSignals(True)
+    slider2.blockSignals(True)
+    slider3.blockSignals(True)
 
     slider.setValue(100)
     slider2.setValue(100)
     slider3.setValue(100)
 
-    rgba_img = new_image.convert("RGBA")
-    data = rgba_img.tobytes("raw", "RGBA")
-    q_img = QImage(data, rgba_img.size[0], rgba_img.size[1], QImage.Format_RGBA8888)
+    slider.blockSignals(False)
+    slider2.blockSignals(False)
+    slider3.blockSignals(False)
 
-    pixmap = QPixmap.fromImage(q_img)
-    image_label.setPixmap(pixmap)
+    file = orig_file.copy()
+    show_img(file)
 
 def open_file():
-    global file
+    global file, orig_file
     file_path, _ = QFileDialog.getOpenFileName(main_win, "Выберите файл", "", "Все файлы (*);;Изображения (*.png *.jpg *.jpeg);")
     if file_path:
         file = Image.open(file_path)
+        orig_file = file.copy()
         print(file_path)
         img_update()
 
 def save_file():
-    global file
+    global file, orig_file
     if file is not None:
         file_path, _ = QFileDialog.getSaveFileName(
             main_win, "Выберите путь", f"new_image.png", "Все файлы (*);;Изображения (*.png *.jpg *.jpeg);"
         )
         if file_path:
-            file.save(file_path)
+            final_image = upd_effects(file)
+            final_image.save(file_path)
             print(f"Файл успешно сохранен: {file_path}")
+
+def rotate_pic_l():
+    global file, orig_file
+    global angle
+    if file is None:
+        return
+
+    file = file.rotate(-90, expand=True)
+    orig_file = orig_file.rotate(-90, expand=True)
+
+    img_update()
+
+def rotate_pic_r():
+    global file, orig_file
+    global angle
+    if file is None:
+        return
+
+    file = file.rotate(90, expand=True)
+    orig_file = orig_file.rotate(90, expand=True)
+
+    img_update()
+
+def mirror_x():
+    global file, orig_file
+    if file is None:
+        return
+
+    file = file.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+    orig_file = orig_file.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
+    img_update()
+
+def mirror_y():
+    global file, orig_file
+    if file is None:
+        return
+
+    file = file.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+    orig_file = orig_file.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+    img_update()
+
 
 app = QApplication([])
 main_win = QWidget()
@@ -118,6 +168,26 @@ btn_clear = QPushButton("Сбросить", main_win)
 btn_clear.setGeometry(525, 560, 190, 35)
 btn_clear.setStyleSheet("background-color: #34495e; color: white; font-weight: bold;")
 btn_clear.clicked.connect(clear_effects)
+
+btn_rr90 = QPushButton("<", main_win)
+btn_rr90.setGeometry(330, 460, 35, 35)
+btn_rr90.setStyleSheet("background-color: #34495e; color: white; font-weight: bold;")
+btn_rr90.clicked.connect(rotate_pic_l)
+
+btn_rl90 = QPushButton(">", main_win)
+btn_rl90.setGeometry(330, 510, 35, 35)
+btn_rl90.setStyleSheet("background-color: #34495e; color: white; font-weight: bold;")
+btn_rl90.clicked.connect(rotate_pic_r)
+
+btn_mrx = QPushButton("-", main_win)
+btn_mrx.setGeometry(380, 460, 35, 35)
+btn_mrx.setStyleSheet("background-color: #34495e; color: white; font-weight: bold;")
+btn_mrx.clicked.connect(mirror_x)
+
+btn_mry = QPushButton("|", main_win)
+btn_mry.setGeometry(380, 510, 35, 35)
+btn_mry.setStyleSheet("background-color: #34495e; color: white; font-weight: bold;")
+btn_mry.clicked.connect(mirror_y)
 
 image_label.setGeometry(0, 0, 750, 450)
 image_label.setScaledContents(True)
